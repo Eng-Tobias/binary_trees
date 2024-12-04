ea49c33375bac2593ff9de42704f1f3ca9f6a83a
Eng-Tobias <engtobiasguya100@gmail.com>
2024-12-04 15:45:38 -0800

    Corrected Betty style issues

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,31 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Inserts a node as the right-child of another node
- * @parent: Pointer to the parent node
- * @value: Value to store in the new node
- *
- * Return: Pointer to the created node, or NULL on failure or if parent is NULL
- */
+* binary_tree_insert_right - Inserts a node as the right-child of another node
+* @parent: Pointer to the parent node
+* @value: Value to store in the new node
+*
+* Return: Pointer to the created node, or NULL on failure or if parent is NULL
+*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return (NULL);
+if (parent == NULL)
+return (NULL);
 
-    /* Create a new node */
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-        return (NULL);
+/* Create a new node */
+new_node = binary_tree_node(parent, value);
+if (new_node == NULL)
+return (NULL);
 
-    /* If parent already has a right child, handle reattachment */
-    new_node->right = parent->right;
-    if (parent->right != NULL)
-        parent->right->parent = new_node;
+/* If parent already has a right child, handle reattachment */
+new_node->right = parent->right;
+if (parent->right != NULL)
+parent->right->parent = new_node;
 
-    /* Attach the new node as the parent's right child */
-    parent->right = new_node;
+/* Attach the new node as the parent's right child */
+parent->right = new_node;
 
-    return (new_node);
+return (new_node);
 }
