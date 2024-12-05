0c7c58542bed4981d119e17335c9b1a04fa03fc5
Eng-Tobias <engtobiasguya100@gmail.com>
2024-12-05 14:24:40 -0800

    Add function to measure the size of a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -34,5 +34,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
