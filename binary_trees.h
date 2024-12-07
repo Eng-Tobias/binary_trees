3e5c12c7b1bdbcc77ad49e1a072ffc269ea9d204
Eng-Tobias <engtobiasguya100@gmail.com>
2024-12-06 16:04:49 -0800

    Implement left rotation on binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -57,5 +57,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_lca(binary_tree_t *tree, binary_tree_t *n1, binary_tree_t *n2);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
