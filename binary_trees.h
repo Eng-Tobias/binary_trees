b7127ba19c8813b5663dcc63eac4c6b96fbbd6ee
Eng-Tobias <engtobiasguya100@gmail.com>
2024-12-06 16:25:40 -0800

    Implement function to check if a binary tree is a valid BST

--- binary_trees.h
+++ binary_trees.h
@@ -59,5 +59,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int is_bst_helper(const binary_tree_t *tree, int min, int max);
+int binary_tree_is_bst(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
